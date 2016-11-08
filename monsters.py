a167ccbccadadc259d712ebc742a405103888752
phlpp77 <phlpp77>
2016-11-08 21:08:49 +0100

    Joker und Premium Stuff
    (Langschwert und Kettenhemd)

--- monsters.py
+++ monsters.py
@@ -9,3 +9,7 @@ class Ork1(Monster): #Handlanger
     def __init__(self):
         Monster.__init__(self,10101,'Ork',False,False,'gfxmnstr/ork1.gif',(0,11,9,25,1,4,0,0,2,20,Noitem()))
 
+class Joker(Monster): #Endboss
+    def __init__(self):
+        Monster.__init__(self,10102, 'Joker',False,False,'gfxmnstr/joker.gif',(0,11,11,40,1,4,0,0,2,20,Noitem()))
+

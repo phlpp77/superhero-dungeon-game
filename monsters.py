951a03d72ebc45d62614d51370feaf5777eb102e
phlpp77 <phlpp77>
2016-11-08 20:16:39 +0100

    Monster = Handlanger

--- monsters.py
+++ monsters.py
@@ -5,7 +5,7 @@ class Nomonster(Monster):
     def __init__(self):
         Monster.__init__(self,10100,'',True,False,'gfxitems/noitem.gif',(0,0,0,0,0,0,0,0,0,0,Noitem()))
 
-class Ork1(Monster):
+class Ork1(Monster): #Handlanger
     def __init__(self):
         Monster.__init__(self,10101,'Ork',False,False,'gfxmnstr/ork1.gif',(0,11,9,25,1,4,0,0,2,20,Noitem()))
 

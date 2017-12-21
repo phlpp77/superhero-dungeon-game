7dac6c01bb197d436ae9f325e216c8374450d60c
Maximilian <maximilian.lohmann.hh@gmail.com>
2017-12-21 17:12:48 +0000

    Revert "Implementation of a music radio"
    This reverts commit f130c939578ad47b7c380da80ae785c5a6d4b128

---
+++ helden/flash.py
@@ -0,0 +1,18 @@
+from helden.held import *
+from items import *
+
+
+class Flash(Held):
+
+    def __init__(self, name):
+        Held.__init__(self, name)
+        self._heldentyp = 5
+        self._typname = 'Flash'  # Typ des Helden
+        self._eigenschaften = [9, 13, 10, 13, 12, 11, 13, 9]  # MU KL CH IN FF GE KO KK
+        self._kampfwerte = [14, 10, 150]  # AT PA LeP
+        self._maxle = self._kampfwerte[2]  # LeP-Maximum
+        self._bild = 'gfxhelden/Flash.gif'
+        self._anzeigeBild = 'gfxhelden/Flash0.gif'
+        self._itemliste = [Dolch(), Kleidung()]  # leere Liste, in der alle Items gespeichert sind
+        self._waffe = self._itemliste[0]  # Waffe
+        self._ruestung = self._itemliste[1]  # Ruestung
--- helden/held.py
+++ helden/held.py
@@ -3,39 +3,25 @@ from items import *
 import time
 
 
-# Class hero
-class Hero:
+class Held:
 
     def __init__(self, name):
         self._heldenname = name
-        self._heldentyp = 0  # 0 Batman, 1 Superman, 2 Spiderman, 3 Ironman, Green Lantern, 4 Flash
+        self._heldentyp = 0  # 0 Batman; 1 Superman; 2 Ironman; 3 Heiler
         self._geschlecht = 0  # 0 maennlich; 1 weiblich
-        self._typname = 'Batman'  # Typ des Heroen
+        self._typname = 'Batman'  # Typ des Helden
         self._eigenschaften = [11, 11, 10, 11, 10, 11, 10, 10]  # MU KL CH IN FF GE KO KK
         self._kampfwerte = [11, 11, 165]  # AT PA LeP
+        self._maxle = self._kampfwerte[2]  # LeP-Maximum
         self._bild = 'gfxhelden/Batman.gif'
         self._anzeigeBild = 'gfxhelden/Batman0.gif'
-        self._x, self._y, self._ap = 0, 0, 0
+        self._x = 0
+        self._y = 0
         self._lichtradius = 1.0
         self._itemliste = [Schwert(), Wattierterwaffenrock()]  # leere Liste, in der alle Items gespeichert sind
-        self._ruestung = self._waffe = self._maxle = 0
-        self.var_init()
-
-    @staticmethod
-    def factory(wanted_obj):
-        text = "Bitten Name eingeben"
-        subclasses = {
-            "Batman": Batman(text),
-            "Superman": Superman(text),
-            "Spiderman": Spiderman(text),
-            "Ironman": Ironman(text),
-            "GreenLantern": GreenLantern(text),
-            "Flash": Flash(text)
-        }
-        return subclasses.get(wanted_obj)
-
-    def var_init(self):
-        self._maxle, self._waffe, self._ruestung = self._kampfwerte[2], self._itemliste[0], self._itemliste[1]
+        self._waffe = self._itemliste[0]  # Waffe
+        self._ruestung = self._itemliste[1]  # Ruestung
+        self._ap = 0
 
     def getheldenname(self):
         return self._heldenname
@@ -97,7 +83,7 @@ class Hero:
     def setbild(self, bild):
         self._bild = bild
 
-    def get_anzeige_bild(self):
+    def getanzeigeBild(self):
         return self._anzeigeBild
 
     def getx(self):
@@ -114,14 +100,14 @@ class Hero:
 
     # noinspection PyAttributeOutsideInit
     def ausleuchten(self):
-        light = [(self._x, self._y)]
+        L = [(self._x, self._y)]
         self.fackelradius = 1.0  # spaeter an anderer Stelle einfuegen, etwa beim Entzueden oder Besitzen einer Fackel
         maxr = self._lichtradius + self.fackelradius
         for i in range(int(8 * maxr)):
             for r in range(int(maxr)):
-                light = light + [(self._x + int(round((r + 1) * cos(2 * pi * (i + 1) / (8 * maxr)), 0)),
-                                  self._y + int(round((r + 1) * sin(2 * pi * (i + 1) / (8 * maxr)), 0)))]
-        return light
+                L = L + [(self._x + int(round((r + 1) * cos(2 * pi * (i + 1) / (8 * maxr)), 0)),
+                          self._y + int(round((r + 1) * sin(2 * pi * (i + 1) / (8 * maxr)), 0)))]
+        return L
 
     def itemnehmen(self, it):  # nimmt das Item it in die Itemliste auf
         self._itemliste = self._itemliste + [it]
@@ -157,75 +143,8 @@ class Hero:
     def heilen(self, sp):
         self._kampfwerte[2] = self._kampfwerte[2] + int(sp * (self._eigenschaften[4] + self._eigenschaften[6]) / 40)
         if self._kampfwerte[2] > self._maxle:
-            self._kampfwerte[2] = self._maxle
+            self._kampfwerte[2] = self.maxle
 
-    @staticmethod
-    def rennen(heldentyp):
+    def rennen(self, heldentyp):
         if heldentyp != 5:
             time.sleep(0.1)
-
-
-# Subclasses for all heros, new ones are added on top of the old ones
-class Flash(Hero):
-    def __init__(self, name):
-        Hero.__init__(self, name)
-        self._heldentyp, self._typname = 5, 'Flash'
-        self._bild, self._anzeigeBild = 'gfxhelden/Flash.gif', 'gfxhelden/Flash0.gif'
-        self._eigenschaften, self._kampfwerte = [9, 13, 10, 13, 12, 11, 13, 9], [14, 10, 150]
-        self._itemliste = [Dolch(), Kleidung()]
-        self.var_init()
-
-
-class GreenLantern(Hero):
-    def __init__(self, name):
-        Hero.__init__(self, name)
-        self._heldentyp, self._typname, self._lichtradius = 4, 'GreenLantern', 2.0
-        self._bild, self._anzeigeBild = 'gfxhelden/GreenLantern.gif', 'gfxhelden/GreenLantern0.gif'
-        self._eigenschaften, self._kampfwerte = [9, 13, 10, 13, 12, 11, 13, 9], [14, 10, 90]
-        self._itemliste = [Dolch(), Kleidung()]
-        self.var_init()
-
-    def heilen(self, sp):
-        self._kampfwerte[2] += int(sp * (self._eigenschaften[4] + self._eigenschaften[6] + 6) / 40)  # 15% heilen Bonus
-        if self._kampfwerte[2] > self._maxle:
-            self._kampfwerte[2] = self._maxle
-
-
-class Ironman(Hero):
-    def __init__(self, name):
-        Hero.__init__(self, name)
-        self._heldentyp, self._typname, self._lichtradius = 2, 'Ironman', 2.0
-        self._bild, self._anzeigeBild = 'gfxhelden/Ironman.gif', 'gfxhelden/Ironman0.gif'
-        self._eigenschaften, self._kampfwerte = [12, 9, 8, 10, 11, 12, 13, 13], [13, 10, 180]
-        self._itemliste = [Langschwert(), Kettenhemd()]
-        self.var_init()
-
-
-class Spiderman(Hero):
-    def __init__(self, name):
-        Hero.__init__(self, name)
-        self._heldentyp, self._typname, self._lichtradius = 2, 'Spiderman', 2.0
-        self._bild, self._anzeigeBild = 'gfxhelden/Spiderman.gif', 'gfxhelden/Spiderman0.gif'
-        self._eigenschaften, self._kampfwerte = [10, 11, 13, 12, 13, 13, 10, 10], [8, 12, 166]
-        self._itemliste = [Kurzschwert(), Wattierterwaffenrock()]
-        self.var_init()
-
-
-class Superman(Hero):
-    def __init__(self, name):
-        Hero.__init__(self, name)
-        self._heldentyp, self._typname = 1, 'Superman'
-        self._bild, self._anzeigeBild = 'gfxhelden/Superman.gif', 'gfxhelden/Superman0.gif'
-        self._eigenschaften, self._kampfwerte = [12, 9, 8, 10, 11, 12, 13, 13], [19, 10, 98]
-        self._itemliste = [Langschwert(), Kettenhemd()]
-        self.var_init()
-
-
-class Batman(Hero):
-    def __init__(self, name):
-        Hero.__init__(self, name)
-        self._heldentyp, self._typname = 0, 'Batman'
-        self._bild, self._anzeigeBild = 'gfxhelden/Batman.gif', 'gfxhelden/Batman0.gif'
-        self._eigenschaften, self._kampfwerte = [11, 11, 10, 11, 10, 11, 10, 10], [11, 11, 165]
-        self._itemliste = [Schwert(), Wattierterwaffenrock()]
-        self.var_init()

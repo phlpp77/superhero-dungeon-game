965409307901e08f270f316012c73bf9889f897d
phlpp77 <PhilippHemkemeyer@iCloud.com>
2018-03-30 23:37:53 +1100

    first things

--- fightsystem.py
+++ fightsystem.py
@@ -1,6 +1,15 @@
 from helden import *
 from monsters import *
 
+
 class Fight:
     def __init__(self, attacker, defender):
         self._attacker, self._defender = attacker, defender
+        self._attacker_characteristics, self._attacker_combat = attacker.geteigenschaften(), attacker.getkampfwerte()
+
+
+# print(f._attacker_characteristics)
+B = Hero(Batman)
+S = Superman
+f = Fight(Batman, Superman)
+B.geteigenschaften()[0]
